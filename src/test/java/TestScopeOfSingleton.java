0dd5ace6e6d30b513b08325af3ab85ca2e29ed87
guotaolee <37661775+guotaolee@users.noreply.github.com>
2020-04-11 16:04:45 +0800

    spring_learn提交 bean的scope（作用域）

---
+++ src/test/java/TestScopeOfSingleton.java
@@ -0,0 +1,23 @@
+import ioc.UnitTestBase;
+import ioc.bean.BeanScope;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class TestScopeOfSingleton extends UnitTestBase {
+
+    public TestScopeOfSingleton() {
+        super("spring-bean-scope-singleton.xml");
+    }
+
+    @Test
+    public void TestScopeOfPrototype() {
+
+        BeanScope beanScope1 = super.getBean("beanScope");
+        System.out.println(beanScope1.hashCode());
+
+        BeanScope beanScope2 = super.getBean("beanScope");
+        System.out.println(beanScope2.hashCode());
+    }
+}
